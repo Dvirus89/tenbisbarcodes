d5b4adbe84a4e85a72581625d4e2a744c30dc712
Dvir Perets <dvirus89@gmail.com>
2022-12-11 02:00:32 +0200

    debug tuning

--- tenbis-report.py
+++ tenbis-report.py
@@ -171,9 +171,9 @@ def get_barcode_order_info(session, order_id, res_id):
     headers = {"content-type": "application/json"}
     headers.update({'user-token': session.user_token})
     response = session.get(endpoint, headers=headers, verify=False)
-    resp_json = json.loads(response.text)
     if(DEBUG):
         print(endpoint + "\r\n" + str(response.status_code) + "\r\n"  + response.text)
+    resp_json = json.loads(response.text)
     used = resp_json['Data']['Vouchers'][0]['Used']
 
     if not used:
